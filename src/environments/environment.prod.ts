c03870c4a63a61aa1e585770c2a21786641fc37c
The Zenith <abdurrahman.aderinto@gmail.com>
2024-08-18 16:11:20 +0100

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  EX_API_KEY: process.env['EX_API_KEY'],
+  // EX_API_KEY: process.env['EX_API_KEY'],
+  EX_API_KEY: '55e203d06743fee65455e71b',
 };
